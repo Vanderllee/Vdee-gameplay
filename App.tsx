5b6ecdfc2db3442fa5f7c1c61ab66b70c2610f1f
Vanderllee <vanderllee@gmail.com>
2021-07-23 17:12:01 -0300

    added project routes

--- App.tsx
+++ App.tsx
@@ -6,8 +6,11 @@ import { useFonts } from 'expo-font';
 import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter'
 import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani'
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
+
 import { StatusBar } from 'react-native';
+import { Background } from './src/components/Background';
+
 
 export default function App() {
 
@@ -25,18 +28,18 @@ export default function App() {
 
 
   return (
-    <>
-      <StatusBar 
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-      />
+    <Background>
+        <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+        />
 
-    <SignIn />
+      <Routes />
 
-    </>
+    </Background>
 
-  );
+  )
 }
 
 
